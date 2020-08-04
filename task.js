689912af8830a474b7f46a6142ba214d51cae7dc
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-08-04 10:56:17 +0300

    mistakes resolved

--- task.js
+++ task.js
@@ -1,17 +1,22 @@
-'use strict'
+'use strict';
 
 let subMenu = Array.from(document.querySelectorAll('ul.menu_sub'))
 
 function closeAllMenus(array, mainMenu) {
     array.forEach(function (menu) {
-        (mainMenu === menu.closest('ul.menu_main')) ? menu.className = 'menu menu_sub' : '';
+        (mainMenu === menu.closest('ul.menu_main')) ? menu.classList.remove('menu_active') : '';
         });
 };
 
 subMenu.forEach(function (menu) {
     menu.previousElementSibling.onclick = function () {
-        closeAllMenus(subMenu, menu.closest('ul.menu_main'));
-        menu.className = 'menu menu_sub menu_active';
-        return false;
+        if (menu.classList.contains('menu_active')) {
+            closeAllMenus(subMenu, menu.closest('ul.menu_main'));
+            return false;
+        } else {
+            closeAllMenus(subMenu, menu.closest('ul.menu_main'));
+            menu.classList.add('menu_active');
+            return false;
         };
+    };
 });
